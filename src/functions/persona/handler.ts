ba9ffeadca945bd8d8db3e8afb1db19f7abe5325
Orlando Yarasca Lupuche <landojm@hotmail.com>
2024-04-24 16:36:14 -0500

    update middyfy

--- src/functions/persona/handler.ts
+++ src/functions/persona/handler.ts
@@ -1,6 +1,6 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayEvent, APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 import { formatJSONResponse } from '@libs/api-gateway';
-import { middyfy } from '@libs/lambda';
+import  middyfy from '@libs/lambda';
 import { v4 } from "uuid";
 import personasService from '../../services'
 import { IntegrationService } from "@services/integration";
@@ -8,32 +8,36 @@ import { VehiculoRequest } from "@model/request.vehiculo";
 import moment from 'moment-timezone';
 
 
-export const getAllPersonas = middyfy(async (): Promise<APIGatewayProxyResult> => {
-    const personas = await personasService.getAllPersonas();
-    return formatJSONResponse ({
-        personas
-    })
+export const getAllPersonas = middyfy(async (_event: APIGatewayEvent,
+    _context: Context): Promise<APIGatewayProxyResult> => {
+    try{
+        const personas = await personasService.getAllPersonas();
+        const listaPersonas = { listaPersonas:personas } ;
+        return formatJSONResponse (200,listaPersonas);
+    }catch(err){
+        return formatJSONResponse (400,err);
+    }
+    
 });
-export const getAllVehiculosSwapi = middyfy(async (): Promise<APIGatewayProxyResult> => {
+export const getAllVehiculosSwapi = middyfy(async (_event: APIGatewayEvent,
+    _context: Context): Promise<APIGatewayProxyResult> => {
     const service = new IntegrationService();
     const vehiculoRequest: VehiculoRequest = { id:""};
     const data = await service.auth(vehiculoRequest);
-    console.log(data);
-    return formatJSONResponse ({    
-        data
-    })
+    return formatJSONResponse (200,data)
 });
-export const createPersona= middyfy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const createPersona= middyfy(async (_event: APIGatewayEvent,
+    _context: Context): Promise<APIGatewayProxyResult> => {
     try {
         const id = v4();
         const peruTime = moment().tz('America/Lima');
         const utcTime = peruTime.format('YYYY-MM-DD HH:mm:ss');
         let requestBody;
-        if (typeof event.body === 'string') {
-            requestBody = JSON.parse(event.body);
+        if (typeof _event.body === 'string') {
+            requestBody = JSON.parse(_event.body);
         } else {
             // Si event.body ya es un objeto, usarlo directamente
-            requestBody = event.body;
+            requestBody = _event.body;
         }
         //const modelPersona = requestBody as Persona;
         const persona = await personasService.createPersona({
@@ -45,15 +49,9 @@ export const createPersona= middyfy(async (event: APIGatewayProxyEvent): Promise
             creado: utcTime,
             estado: true
         })
-        return formatJSONResponse({
-            persona
-        });
+        return formatJSONResponse(201, persona);
     } catch (e) {
-        console.error('Error al analizar el cuerpo del evento:', e);
 
-        return formatJSONResponse({
-            status: 500,
-            message: e
-        });
+        return formatJSONResponse(500,e);
     }
 });
--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,7 +1,10 @@
 import middy from "@middy/core"
 import middyJsonBodyParser from "@middy/http-json-body-parser"
+import { Handler } from "aws-lambda";
+import cors from "@middy/http-cors";
 
-export const middyfy = (handler:any) => {
-  console.log(handler);
-  return middy(handler).use(middyJsonBodyParser())
+const middyfy = (handler:Handler) => {
+  //console.log(handler);
+  return middy(handler).use(middyJsonBodyParser()).use(cors());
 }
+export default middyfy;
